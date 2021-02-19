acb2cf9ac1b923d7a64394c00029a54afa1c3897
bharani <bharani.developer@gmail.com>
2021-02-19 12:35:11 +0530

    Layout finish

---
+++ includes/bottom.php
@@ -0,0 +1,31 @@
+
+  <!-- BEGIN VENDOR JS-->
+  <script src="assets/js/vendors.min.js" type="text/javascript"></script>
+  <!-- BEGIN VENDOR JS-->
+  <!-- BEGIN PAGE VENDOR JS-->
+  <script src="assets/js/jquery.knob.min.js" type="text/javascript"></script>
+  <script src="assets/js/knob.min.js" type="text/javascript"></script>
+  <script src="assets/js/raphael-min.js" type="text/javascript"></script>
+  <script src="assets/js/morris.min.js" type="text/javascript"></script>
+  <script src="assets/js/jquery-jvectormap-2.0.3.min.js" type="text/javascript"></script>
+  <script src="assets/js/jquery-jvectormap-world-mill.js" type="text/javascript"></script>
+  <script src="assets/js/visitor-data.js" type="text/javascript"></script>
+  <script src="assets/js/chart.min.js" type="text/javascript"></script>
+  <script src="assets/js/jquery.sparkline.min.js" type="text/javascript"></script>
+  <script src="assets/js/unslider-min.js" type="text/javascript"></script>
+  <link rel="stylesheet" type="text/css" href="assets/css/palette-climacon.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/fonts/simple-line-icons/style.min.css">
+  <!-- END PAGE VENDOR JS-->
+
+  <!-- BEGIN STACK JS-->
+  <script src="assets/js/app-menu.min.js" type="text/javascript"></script>
+  <script src="assets/js/app.min.js" type="text/javascript"></script>
+  <script src="assets/js/customizer.min.js" type="text/javascript"></script>
+  <!-- END STACK JS-->
+
+  <!-- BEGIN PAGE LEVEL JS-->
+  <script src="assets/js/dashboard-analytics.min.js" type="text/javascript"></script>
+  <!-- END PAGE LEVEL JS-->
+</body>
+
+</html>
---
+++ includes/tob.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+  <meta name="description" content="Stack admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+  <meta name="keywords" content="admin template, stack admin template, dashboard template, flat admin template, responsive admin template, web app">
+  <meta name="author" content="PIXINVENT">
+  <title>Analytics Dashboard - Stack Responsive Bootstrap 4 Admin Template</title>
+  <link rel="apple-touch-icon" href="assets/images/icon/apple-icon-120.png">
+  <link rel="shortcut icon" type="image/x-icon" href="assets/images/icon/favicon.ico">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+  <!-- BEGIN VENDOR CSS-->
+  <link rel="stylesheet" type="text/css" href="assets/css/vendors.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/jquery-jvectormap-2.0.3.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/morris.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/unslider.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/climacons.min.css">
+  <!-- END VENDOR CSS-->
+  <!-- BEGIN STACK CSS-->
+  <link rel="stylesheet" type="text/css" href="assets/css/app.min.css">
+  <!-- END STACK CSS-->
+  <!-- BEGIN Page Level CSS-->
+  <link rel="stylesheet" type="text/css" href="assets/css/vertical-menu.min.css">
+  <!-- link(rel='stylesheet', type='text/css', href=app_assets_path+'/css'+rtl+'/pages/users.css')-->
+  <!-- END Page Level CSS-->
+  <!-- BEGIN Custom CSS-->
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <!-- END Custom CSS-->
+</head>
+
+<body class="vertical-layout vertical-menu 2-columns   menu-expanded fixed-navbar" data-open="click" data-menu="vertical-menu" data-col="2-columns">
+ 
